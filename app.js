697423dd4d6e1cb792abe80a7a4e210e3e01e532
201steve <79638133+201steve@users.noreply.github.com>
2022-07-27 09:15:19 +0900

    lecture/3-4

--- app.js
+++ app.js
@@ -4,32 +4,39 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
+const getData = (url) => {
+  ajax.open("GET", url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+};
 
-const newsFeeds = JSON.parse(ajax.response);
+const newsFeeds = getData(NEWS_URL);
 const ul = document.createElement("ul");
 
 window.addEventListener("hashchange", () => {
   const id = location.hash.substring(1);
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
 
-  const newsContent = JSON.parse(ajax.response);
+  const newsContents = getData(CONTENT_URL.replace("@id", id));
+
   const title = document.createElement("h1");
-  title.innerHTML = newsContent.title;
+  title.innerHTML = newsContents.title;
   content.appendChild(title);
 });
 
 for (let i = 0; i < 10; i++) {
+  const div = document.createElement("div");
   const li = document.createElement("li");
   const a = document.createElement("a");
 
-  a.href = `#${newsFeeds[i].id}`;
-  a.innerHTML = `${newsFeeds[i].title} (${newsFeeds[i].comments_count})`;
+  div.innerHTML = `
+  <li>
+    <a href="#${newsFeeds[i].id}">
+      ${newsFeeds[i].title} (${newsFeeds[i].comments_count})
+    </a>
+  </li>
+  `;
 
-  li.appendChild(a);
-  ul.appendChild(li);
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
